2a70741ccf8267cb3ba084e2d12087ca93e26896
X1vova1X <128622578+X1vova1X@users.noreply.github.com>
2024-07-14 22:53:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,43 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
+// Helper function to fetch data from the external URLs
+async function fetchData(slot) {
+  try {
+    const response = await axios.get(`http://d90930x1.beget.tech/PocketCodeDB/DATABASE/echo/echo.php?slot=${slot}&token=46609766713SoreAlpha54188945224`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching data for slot ${slot}:`, error);
+    throw error;
+  }
+}
+
+// Endpoint to fetch data for slot 7
 app.get('/server1', async (req, res) => {
   try {
-    const response = await axios.get('http://d90930x1.beget.tech/PocketCodeDB/DATABASE/echo/echo.php?slot=7&token=46609766713SoreAlpha54188945224');
-    res.send(response.data);
+    const data = await fetchData(7);
+    res.send(data);
   } catch (error) {
-    console.error('Error fetching data:', error);
     res.status(500).send('Error fetching data');
   }
 });
 
+// Endpoint to fetch data for slot 8
 app.get('/server2', async (req, res) => {
   try {
-    const response = await axios.get('http://d90930x1.beget.tech/PocketCodeDB/DATABASE/echo/echo.php?slot=8&token=46609766713SoreAlpha54188945224');
-    res.send(response.data);
+    const data = await fetchData(8);
+    res.send(data);
   } catch (error) {
-    console.error('Error fetching data:', error);
     res.status(500).send('Error fetching data');
   }
 });
 
+// Endpoint to fetch data for slot 9
 app.get('/server3', async (req, res) => {
   try {
-    const response = await axios.get('http://d90930x1.beget.tech/PocketCodeDB/DATABASE/echo/echo.php?slot=9&token=46609766713SoreAlpha54188945224');
-    res.send(response.data);
+    const data = await fetchData(8);
+    res.send(data);
   } catch (error) {
-    console.error('Error fetching data:', error);
     res.status(500).send('Error fetching data');
   }
 });
